f15c4d8fbabe98d298a4852647c0ce16861b2e83
ohyeeune <s2018s33@e-mirim.hs.kr>
2020-05-19 17:54:08 +0900

    변수를 가지는 클래스 만들고, 값을 넣어 인스턴스 생성

--- mybook/Program.cs
+++ mybook/Program.cs
@@ -8,8 +8,30 @@ namespace mybook
 {
     class Program
     {
+        class Book
+        {
+            public string name;
+            public string firstBook;
+            public string author;
+            public string publisher;
+            public string publish;
+            public string Seditor;
+            public string planner;
+            public string editor;
+            public string designer;
+        }
         static void Main(string[] args)
         {
+            Book book = new Book();
+            book.name = "PHP 프로그래밍 입문";
+            book.firstBook = "2013년 5월 20일";
+            book.author = "황재호";
+            book.publisher = "김태헌";
+            book.publish = "한빛아카데미(주)";
+            book.Seditor = "김현용";
+            book.planner = "김이화";
+            book.editor = "김이화";
+            book.designer = "여동일";
         }
     }
 }
